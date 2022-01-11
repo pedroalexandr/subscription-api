ae1ccf3b113c135a20df2433fdc8d850a1930ba5
Alexandre <pedro.alexandre@hp.com>
2022-01-11 11:20:10 -0300

    startup: inital commit

---
+++ src/main/kotlin/com/iban/subscriptionsapi/SubscriptionsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.iban.subscriptionsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/iban/subscriptionsapi/SubscriptionsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iban.subscriptionsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubscriptionsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
