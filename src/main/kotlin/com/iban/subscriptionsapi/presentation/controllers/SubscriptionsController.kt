d55c4c46dcc33178d71cb11f6f5adad173862d10
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-13 09:12:10 -0300

    refactor: main controller renaming

--- src/main/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionsController.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionsController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/subscriptions")
-class SubscriptionController(
+class SubscriptionsController(
     private val subscriptionRepository: SubscriptionRepository,
     private val emailService: EmailServiceImpl
 ) {
