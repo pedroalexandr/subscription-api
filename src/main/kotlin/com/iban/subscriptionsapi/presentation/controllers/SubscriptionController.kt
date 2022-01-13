3d6afbceffc5d6e7b5ff33ac2c7181c665aed6f5
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-13 01:19:52 -0300

    refactor: api route re-mapping to use prefix

--- src/main/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionController.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/subscriptions")
+@RequestMapping("/api/v1/subscriptions")
 class SubscriptionController(
     private val subscriptionRepository: SubscriptionRepository,
     private val emailService: EmailServiceImpl
