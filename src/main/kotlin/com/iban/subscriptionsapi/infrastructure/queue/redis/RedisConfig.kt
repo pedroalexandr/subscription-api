f5d0b3c817f589b49f09882c8921b6c31c1d91ac
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-15 12:06:23 -0300

    feat: add redis configuration class

---
+++ src/main/kotlin/com/iban/subscriptionsapi/infrastructure/queue/redis/RedisConfig.kt
@@ -0,0 +1,44 @@
+package com.iban.subscriptionsapi.infrastructure.queue.redis
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.listener.ChannelTopic
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+
+@Configuration
+class RedisConfig() {
+
+    @Value("\${spring.redis.host}")
+    lateinit var redisHost: String
+
+    @Value("\${spring.redis.port}")
+    lateinit var redisPort: String
+
+    @Value("\${spring.redis.topic}")
+    lateinit var redisTopic: String
+
+    @Bean
+    fun jedisConnectionFactory(): JedisConnectionFactory {
+        val config = RedisStandaloneConfiguration(redisHost, redisPort.toInt())
+        val jedisClientConfiguration = JedisClientConfiguration.builder().usePooling().build()
+        val factory = JedisConnectionFactory(config, jedisClientConfiguration)
+        factory.afterPropertiesSet()
+        return factory
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val template: RedisTemplate<String, Any> = RedisTemplate()
+        template.setConnectionFactory(jedisConnectionFactory())
+        template.valueSerializer = GenericJackson2JsonRedisSerializer()
+        return template
+    }
+
+    @Bean
+    fun topic(): ChannelTopic = ChannelTopic(redisTopic)
+}
