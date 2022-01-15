59e171cc1637dcf932d5562372b10d62fa623e53
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-15 12:07:29 -0300

    test: refactor SubscriptionsController tests

--- src/test/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionsControllerTests.kt
+++ src/test/kotlin/com/iban/subscriptionsapi/presentation/controllers/SubscriptionsControllerTests.kt
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.github.javafaker.Faker
 import com.iban.subscriptionsapi.domain.models.Subscription
+import com.iban.subscriptionsapi.helpers.SubscriptionFactory
 import com.iban.subscriptionsapi.infrastructure.db.SubscriptionRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -26,24 +28,15 @@ class SubscriptionsControllerTests {
     lateinit var mockMvc: MockMvc
 
     @Autowired
-    lateinit var subscriptionRepository: SubscriptionRepository
+    lateinit var jpaRepository: JpaRepository<Subscription, Long>
 
     private val mapper = ObjectMapper()
         .registerModule(JavaTimeModule())
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
-    private val faker = Faker()
-
     @Test
     fun `should create a subscription successfully`() {
-        val subscription = Subscription(
-            email = faker.internet().emailAddress(),
-            firstName = faker.name().firstName(),
-            dateOfBirth = LocalDate.now(),
-            optIn = true,
-            newsletterId = 1
-        )
-
+        val subscription = SubscriptionFactory().create()
         val stringifiedSubscription = mapper.writeValueAsString(subscription)
 
         mockMvc.perform(
@@ -59,19 +52,13 @@ class SubscriptionsControllerTests {
             .andExpect(MockMvcResultMatchers.jsonPath("\$.optIn").value(subscription.optIn))
             .andExpect(MockMvcResultMatchers.jsonPath("\$.newsletterId").value(subscription.newsletterId))
 
-        Assertions.assertFalse(subscriptionRepository.findAll().isEmpty())
+        Assertions.assertFalse(jpaRepository.findAll().isEmpty())
     }
 
     @Test
     fun `should return bad request status when any required parameter is not provided`() {
-        val subscriptionMissingEmailField = object {
-            val firstName = faker.name().firstName()
-            val dateOfBirth = LocalDate.now()
-            val optIn = true
-            val newsletterId = 1
-        }
-
-        val stringifiedSubscription = mapper.writeValueAsString(subscriptionMissingEmailField)
+        val subscriptionMissingRequiredField = SubscriptionFactory().createPartial()
+        val stringifiedSubscription = mapper.writeValueAsString(subscriptionMissingRequiredField)
 
         mockMvc.perform(
             MockMvcRequestBuilders.post(CONTROLLER_ROUTE)
@@ -81,6 +68,6 @@ class SubscriptionsControllerTests {
         )
             .andExpect(MockMvcResultMatchers.status().isBadRequest)
 
-        Assertions.assertTrue(subscriptionRepository.findAll().isEmpty())
+        Assertions.assertTrue(jpaRepository.findAll().isEmpty())
     }
 }
