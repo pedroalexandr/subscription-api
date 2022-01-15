55c9773e7ea48e09b29eefb0b6b74a8f2ba608fb
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-15 12:03:41 -0300

    refactor: change domain models folder name

--- src/main/kotlin/com/iban/subscriptionsapi/domain/models/Subscription.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/domain/models/Subscription.kt
@@ -1,17 +1,24 @@
-package com.iban.subscriptionsapi.domain.model
+package com.iban.subscriptionsapi.domain.models
 
+import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
+enum class Gender(val char: Char) {
+    MALE('M'),
+    FEMALE('F'),
+    OTHER('O'),
+}
+
 @Entity(name = "subscriptions")
 data class Subscription(
     @Id @GeneratedValue
-    val id: Long,
+    val id: Long? = null,
     val email: String,
     val firstName: String = "",
-    val gender: String = "",
-    val dateOfBirth: String,
+    val gender: Char = Gender.OTHER.char,
+    val dateOfBirth: LocalDate,
     val optIn: Boolean,
     val newsletterId: Long,
 )
