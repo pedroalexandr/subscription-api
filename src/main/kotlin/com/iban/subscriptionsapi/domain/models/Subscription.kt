80e88a73a8764053fb21fa9cc1839d633aadbc1e
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-15 22:21:15 -0300

    refactor: add validations to the subscription entity's fields

--- src/main/kotlin/com/iban/subscriptionsapi/domain/models/Subscription.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/domain/models/Subscription.kt
@@ -4,21 +4,29 @@ import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
-
-enum class Gender(val char: Char) {
-    MALE('M'),
-    FEMALE('F'),
-    OTHER('O'),
-}
+import javax.validation.constraints.*
 
 @Entity(name = "subscriptions")
-data class Subscription(
+class Subscription(
     @Id @GeneratedValue
-    val id: Long? = null,
+    private val id: Long? = null,
+
+    @field:Email(message = "Invalid email")
+    @field:NotBlank(message = "Email is required")
     val email: String,
-    val firstName: String = "",
-    val gender: Char = Gender.OTHER.char,
+
+    val firstName: String? = null,
+
+    @field:Pattern(regexp = "[MFO]", message = "Invalid gender. Must be one of the following: M, F or O")
+    val gender: String? = null,
+
+    @field:Past(message = "The date of birth must be in the past")
+    @field:NotNull(message = "Date of birth is required")
     val dateOfBirth: LocalDate,
+
     val optIn: Boolean,
+
+    @field:NotNull(message = "A newsletter related is required")
+    @field:Min(1, message = "Invalid id for related newsletter")
     val newsletterId: Long,
 )
