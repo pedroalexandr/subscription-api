57225afc7806480e73a0db648d25dd7792f4da24
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-16 19:55:58 -0300

    refactor: RedisQueueMessagePublisher implementation to simply use Jedis

--- src/main/kotlin/com/iban/subscriptionsapi/infrastructure/queue/redis/RedisQueueMessagePublisher.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/infrastructure/queue/redis/RedisQueueMessagePublisher.kt
@@ -1,17 +1,22 @@
 package com.iban.subscriptionsapi.infrastructure.queue.redis
 
+import com.google.gson.Gson
 import com.iban.subscriptionsapi.application.protocols.QueueMessagePublisher
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.listener.ChannelTopic
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import redis.clients.jedis.Jedis
 
 @Component
-class RedisQueueMessagePublisher(
-    private val redisTemplate: RedisTemplate<String, Any>,
-    private val channelTopic: ChannelTopic
-) : QueueMessagePublisher {
-    override fun publish(message: Any): Void? {
-        redisTemplate.convertAndSend(channelTopic.topic, message)
+class RedisQueueMessagePublisher : QueueMessagePublisher {
+    @Value("\${spring.redis.host}")
+    private lateinit var redisHost: String
+
+    @Value("\${spring.redis.port}")
+    private lateinit var redisPort: String
+
+    override fun publish(key: String, message: Any): Void? {
+        val jedis = Jedis(redisHost, redisPort.toInt())
+        jedis.rpush(key, Gson().toJson(message))
         return null
     }
 }
