e0b1c8e47df9996954f5dae1eca5a23d5ab67f9c
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-15 22:20:41 -0300

    feat: add global exception handler and override common bad request method

---
+++ src/main/kotlin/com/iban/subscriptionsapi/RestResponseEntityExceptionHandler.kt
@@ -0,0 +1,29 @@
+package com.iban.subscriptionsapi
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        val errors: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.getDefaultMessage()
+            errors[fieldName] = errorMessage
+        }
+        return ResponseEntity<Any>(errors, HttpStatus.BAD_REQUEST)
+    }
+}
