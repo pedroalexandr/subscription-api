7f6029108ace697bf86d1884fb6cf30f91962942
Pedro Alexandre <alexdevcoder@gmail.com>
2022-01-16 00:26:39 -0300

    feat: change swagger url server

--- src/main/kotlin/com/iban/subscriptionsapi/SubscriptionsApiApplication.kt
+++ src/main/kotlin/com/iban/subscriptionsapi/SubscriptionsApiApplication.kt
@@ -1,8 +1,13 @@
 package com.iban.subscriptionsapi
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+	servers = [Server(url = "/")]
+)
 @SpringBootApplication
 class SubscriptionsApiApplication
 
